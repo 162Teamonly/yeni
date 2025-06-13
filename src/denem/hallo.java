b5a6384c0efdb20de015bb78fae20a67a7591f57
karahan <karahanramazan@yahoo.com>
2025-06-13 21:53:20 +0200

    first commit

---
+++ src/denem/hallo.java
@@ -0,0 +1,7 @@
+package denem;
+
+public class hallo {
+    public static void main(String[] args) {
+        System.out.println("hallo");
+    }
+}
---
+++ src/denem/test.java
@@ -0,0 +1,7 @@
+package denem;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
