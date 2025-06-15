68d4cb54dbe9f780004b61f30cf56de31b95b2b6
karahan <karahanramazan@yahoo.com>
2025-06-15 19:45:51 +0200

    3.gun

--- src/denem/test.java
+++ src/denem/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test111");
+        System.out.println("yeni bir gun");
     }
 }
